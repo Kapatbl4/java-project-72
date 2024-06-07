7725b6fc9cb683e7b4d276c0a52941614b545b5f
Kapatbl4 <vvi93@yandex.ru>
2024-06-07 16:48:58 +0300

    add urlCheck test

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,13 +1,13 @@
 package hexlet.code;
 
-//import hexlet.code.model.Url;
-//import hexlet.code.model.UrlCheck;
-//import hexlet.code.repository.UrlCheckRepository;
-//import hexlet.code.repository.UrlsRepository;
-//import hexlet.code.util.NamedRoutes;
+import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+import hexlet.code.repository.UrlCheckRepository;
+import hexlet.code.repository.UrlsRepository;
+import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
-//import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,8 +15,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
 
 import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -25,17 +25,16 @@ public class AppTest {
 
     private static Javalin app;
     private static MockWebServer mockServer;
-    private static String mockUrl;
 
-    @BeforeAll
-    public static void beforeAll() {
-        mockServer = new MockWebServer();
-        mockUrl = mockServer.url("/").toString();
-    }
 
     @BeforeEach
     public final void setUp() throws SQLException, IOException {
         app = App.getApp();
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        mockServer = new MockWebServer();
     }
 
     @Test
@@ -103,25 +102,26 @@ public class AppTest {
         });
     }
 
-//    @Test
-//    public void testCheckUrl() throws IOException {
-//        String mockResponse = Files.readString(Paths.get("src/test/resources/mockTest.html"));
-//        mockServer.enqueue(new MockResponse()
-//                .setBody(mockResponse));
-//        JavalinTest.test(app, (server, client) -> {
-//            Url url = new Url(mockUrl);
-//            UrlsRepository.save(url);
-//            var response = client.post(NamedRoutes.urlCheckPath(url.getId()));
-//            assertThat(response.code()).isEqualTo(200);
-//
-//            UrlCheck urlCheck = UrlCheckRepository.getLast(url.getId()).get();
-//
-//            var title = urlCheck.getTitle();
-//            var h1 = urlCheck.getH1();
-//
-//            assertThat(title).isEqualTo("Test");
-//            assertThat(h1).isEqualTo("Test is successful");
-//            mockServer.shutdown();
-//        });
-//    }
+    @Test
+    public void testCheckUrl() throws IOException {
+        String mockResponse = Files.readString(Paths.get("src/test/resources/mockTest.html"));
+        mockServer.enqueue(new MockResponse()
+                .setBody(mockResponse));
+        JavalinTest.test(app, (server, client) -> {
+            Url url = new Url(mockServer.url("/").toString());
+            UrlsRepository.save(url);
+            Url savedUrl = UrlsRepository.findByName(mockServer.url("/").toString()).orElseThrow();
+            var response = client.post(NamedRoutes.urlCheckPath(savedUrl.getId()));
+            assertThat(response.code()).isEqualTo(200);
+
+            UrlCheck urlCheck = UrlCheckRepository.getLast(savedUrl.getId()).get();
+
+            var title = urlCheck.getTitle();
+            var h1 = urlCheck.getH1();
+
+            assertThat(title).isEqualTo("Test");
+            assertThat(h1).isEqualTo("Test is successful");
+            mockServer.shutdown();
+        });
+    }
 }
